a829290a720a83e78f33744125ed41cb3fb0db9d
dy <dy@121.136.3.39>
2018-07-27 00:49:38 +0900

    realtree하는중

--- WebContent/dept/realtree.jsp
+++ WebContent/dept/realtree.jsp
@@ -18,6 +18,7 @@
 <script src="/kimsaemERP/common/js/query.cookie.js"></script>
 <script type="text/javascript" src="/kimsaemERP/common/js/demo.js"></script>
 <script type="text/javascript">
+	ulnode="";
 	$(document).ready(function() {
 		$(".folder").on("click", function() {
 			deptname = $(this).text().trim();
@@ -32,7 +33,9 @@
 		//1번매개변수 - 이벤트시점, 2번매개변수 - 어떤 태그에 이벤트를 연결할 것 인지 정의
 		//3번매개변수 - 이벤트가 발생할 때 실행할 함수
 		$(document).on("click",".file",function(){
-			alert("test");
+			id = "";
+			alert(id);
+			$.get("/kimsaemERP/getempinfo.do", {"id":id}, getEmpInfo, "json");
 		})
 	})
 	//jquery를 이용하면 json이 파싱되어 객체상태로 리턴된다.
@@ -44,6 +47,17 @@
 					+ data.emplist[i].name +"</span></li>";
 		}
 		$(ulnode).html(myli);
+	}
+	function getEmpInfo(emp){
+		alert("test");
+		$("#empinfo").css("display","block");
+		$("#userImage").attr("src","/kimsaemERP/images/"+emp.profile_photo);
+		$("#deptno").text(emp.deptno);
+		$("#name").text(emp.name);
+		$("#id").text(emp.id);
+		$("#position").text(emp.position);
+		$("#duty").text(emp.duty);
+		
 	}
 </script>
 </head>
@@ -66,5 +80,66 @@
 			</li>
 		<%} %>
 	</ul>
+	
+	<div style="margin-top: 20px; height: 400px; display:none;" class="col-sm-5" id="empinfo" >
+		<form role="form" class="form-horizontal"
+			action="/kimsaemERP/emp/update.do" method="POST" name="myform">
+			<fieldset>
+				<div id="legend">
+					<legend>선택한 사원의 기본 정보입니다.</legend>
+				</div>
+				<div class="form-group">
+					<p class="col-sm-4">
+						<img src="" id="userImage" style="width: 90px">
+					</p>
+					<div class="col-sm-8" style="color: blue;">직원 정보가 보여지는 곳으로
+						수정을 원하시면 부서별인사조회를 선택하고 작업하세요.</div>
+
+				</div>
+				<div class="form-group">
+					<!-- 부서코드 -->
+					<label class="control-label col-sm-4" for="orgcode">부서코드</label>
+					<div class="control-label col-sm-4" id="deptno">
+						<!-- 여기에 부서코드를 출력하세요  -->
+					</div>
+				</div>
+				<div class="form-group">
+						<!-- 부서코드 -->
+						<label class="control-label col-sm-4" for="orgcode">부서명</label>
+						<div class="control-label col-sm-3">
+							<!-- 여기에 부서명을 출력하세요  -->
+						</div>
+				</div>
+				<div class="form-group">
+					<!-- 성명-->
+					<label class="control-label col-sm-4" for="name">성명</label>
+					<div class="control-label col-sm-4" id="name">
+					</div>
+				</div>
+				<div class="form-group">
+					<!-- 사번-->
+					<label class="control-label col-sm-4" for="id">사번</label>
+					<div class="control-label col-sm-4" id="id">
+						<!-- 여기에 사번을 출력하세요  -->
+					</div>
+					<span id="checkVal"></span>
+				</div>
+				<div class="form-group">
+					<!-- 직위-->
+					<label class="control-label col-sm-4" for="birthday">직위</label>
+					<div class="control-label col-sm-4" id="position">
+						<!-- 여기에 직위 출력하세요  -->
+					</div>
+				</div>
+				<div class="form-group">
+					<!-- 직책-->
+					<label class="control-label col-sm-4" for="birthday">직책</label>
+					<div class="control-label col-sm-4" id="duty">
+						<!-- 여기에 직책 출력하세요  -->
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
 </body>
 </html>
---
+++ build/classes/query/step2.sql
@@ -0,0 +1,73 @@
+insert into member values('881011kim','1234','김동현','721012-1111111','1990/11/11','1','1','과장','팀장','4급20호봉','2010/10/01',null,'d006','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','kimdong.jpg');
+insert into member values('787871lee','1234','이민호','721012-1111111','1990/11/11','1','1','대리','사원','4급20호봉','2010/10/01',null,'dg001','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','leemin.jpg');
+insert into member values('17171park','1234','박문수','721012-1111111','1990/11/11','1','1','대리','사원','4급20호봉','2010/10/01',null,'d0032','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','park.jpg');
+insert into member values('9871kim','1234','김어준','721012-1111111','1990/11/11','1','1','과장','팀장','4급20호봉','2010/10/01',null,'d009','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','jjangkim.jpg');
+insert into member values('lee0idjj','1234','안정환','721012-1111111','1990/11/11','1','1','차장','팀장','4급20호봉','2010/10/01',null,'dg002','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','ansoccer.jpg');
+insert into member values('djdiwjs','1234','차범근','721012-1111111','1990/11/11','1','1','사원','사원','4급20호봉','2010/10/01',null,'dg003','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','chasoccer.jpg');
+insert into member values('282djdid','1234','박지성','721012-1111111','1990/11/11','1','1','대리','파트장','4급20호봉','2010/10/01',null,'d001','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','parksoccer.jpg');
+insert into member values('Yeona1231','1234','김연아','721012-1111111','1990/11/11','1','1','대리','파트장','4급20호봉','2010/10/01',null,'d003','재직','222-222','서울시 봉천구','좋은동네','02-858-1111','02-858-1111','010-111-4444','jang@naver.com','Yeona.jpg');
+
+
+create sequence purchase_seq;
+create sequence purchase_detail_seq;
+
+drop sequence purchase_seq;
+drop sequence purchase_detail_seq;
+drop table purchase
+drop table purchase_detail
+
+#구매일반
+create table purchase(
+	purchase_id varchar2(20) primary key,
+	purchase_day date,
+	request_id varchar2(15),
+	request_dept varchar2(20),
+	request_deptno varchar2(20),
+	state char(1),
+	type varchar2(15)
+)
+
+#구매상세
+create table purchase_detail(
+    purchase_id varchar2(20),
+	detail_num number,
+	prd_code varchar2(15),
+	prd_name varchar2(15),
+	standard varchar2(15),
+	count number,
+	price number,
+	supplyvalue number,
+	vat number,
+	totalprice number,
+	clientcode varchar2(20)
+)
+
+insert into purchase values(to_char(sysdate,'yyyymmdd')|| purchase_seq.nextval, sysdate,'787871lee', '경영지원본부','0','사내행사용');
+insert into purchase_detail values(to_char(sysdate,'yyyymmdd')||purchase_seq.currval,purchase_detail_seq.nextval,'prd_002','현수막','4*10',2,100000,200000,20000,220000,'code001');
+insert into purchase_detail values(to_char(sysdate,'yyyymmdd')||purchase_seq.currval,purchase_detail_seq.nextval,'prd_003','의자','',20,5000,100000,10000,110000,'code010');
+insert into purchase_detail values(to_char(sysdate,'yyyymmdd')||purchase_seq.currval,purchase_detail_seq.nextval,'prd_010','탁자','5*10',1,100000,100000,10000,110000,'code0002');
+
+
+insert into purchase values(to_char(sysdate,'yyyymmdd')|| purchase_seq.nextval, sysdate,'Yeona1231', '개발팀','0','개발용');
+insert into purchase_detail values(to_char(sysdate,'yyyymmdd')||purchase_seq.currval,purchase_detail_seq.nextval,'prd_00i2','노트북','18인치',5,865000,4325000,432500,4757500,'com001');
+insert into purchase_detail values(to_char(sysdate,'yyyymmdd')||purchase_seq.currval,purchase_detail_seq.nextval,'prd_00i3','오피스','ver2016',10,30000,300000,30000,330000,'soft010');
+insert into purchase_detail values(to_char(sysdate,'yyyymmdd')||purchase_seq.currval,purchase_detail_seq.nextval,'prd_0i10','펜마우스','11t',1,100000,100000,10000,110000,'pen0002');
+
+select * from member;
+
+select * from purchase;
+select * from purchase_detail;
+
+select rownum, p.*
+from (select  * from purchase
+	  where state ='0' 
+      order by purchase_day desc) p
+where rownum=1
+
+
+
+
+
+
+
+
